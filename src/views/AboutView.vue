9558b197a1ba86e7ef4c8fd32428bf692b0af11b
MJ <marijalaghonia@gmail.com>
2024-09-20 21:48:57 +0400

    delete function added, back button added.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,10 @@
   <content>
     <div class="container-fluid">
       <div class="row">
+        <div class="col-sm-12 mb-3 mb-sm-0" @click="returnBack">
+          <img src="../assets/Icon_right.png" />
+        </div>
+
         <div class="col-sm-6 mb-3 mb-sm-0">
           <img class="img-fluid" :src="cityes.image" style="width: 100%" />
         </div>
@@ -45,6 +49,19 @@
             <div class="mt-2">
               {{ cityes.description }}
             </div>
+
+            <div class="row justify-content-end mt-5">
+              <div class="col-auto">
+                <button
+                  @click="openModal"
+                  type="button"
+                  class="btn btn-outline-danger btn-outline-custom"
+                  style="border-color: #f93b1d"
+                >
+                  წაშლა
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -53,15 +70,34 @@
 </template>
 
 <script setup>
-import { reactive, onMounted, getCurrentInstance, ref, watch } from 'vue'
+import { onMounted, getCurrentInstance, ref } from 'vue'
 
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const instance = getCurrentInstance()
 const route = useRoute() // Access route information
 const itemId = route.params.id
 
 const cityes = ref([])
+
+const openModal = () => {
+  instance.appContext.app
+    .axios({
+      method: 'delete',
+      baseURL: 'https://api.real-estate-manager.redberryinternship.ge/api/real-estates/' + itemId,
+      headers: { Authorization: `Bearer 9d0d90d2-e455-4c79-b702-7db764c3a6db` }
+    })
+    .then((response) => {
+      cityes.value = response.data
+      router.go(-1)
+    })
+}
+
+const returnBack = () => {
+  router.go(-1)
+}
 
 onMounted(() => {
   instance.appContext.app
