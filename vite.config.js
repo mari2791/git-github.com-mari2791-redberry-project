2d081a0325900bfc604de87b4b232f6ffd66e366
MJ <marijalaghonia@gmail.com>
2024-09-21 15:46:09 +0400

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/redberry-project/',
+  base: '/redberry-project-1/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
